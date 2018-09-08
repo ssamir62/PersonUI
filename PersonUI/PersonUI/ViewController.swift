01b7752510389b48575b9e96c45748377e352703
Sharoze Amir <ssamir122@gmail.com>
2018-09-07 21:50:16 -0500

    Person UI

---
+++ PersonUI/PersonUI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PersonUI
+//
+//  Created by Sharoze Amir on 9/7/18.
+//  Copyright © 2018 Sharoze Amir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
